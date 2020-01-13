5bc5859b5b9f8455cd2e28f07e83e854179f671e
sayid <herq12315@gmail.com>
2020-01-13 16:19:56 +0800

    update echo format

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,7 @@ if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
   cd _site
   rm CNAME
   touch README.md
-  echo "
-    # 快速 setup node server
+  echo "# 快速 setup node server
 
     初始化目录
     ```
@@ -20,8 +19,7 @@ if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
     开启 `http server`
     ```
     http-server [path] [options]
-    ```
-  " > README.md
+    ```" > README.md
 
   git init
 
