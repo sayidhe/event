aefd496c4255721a196d8981ebcb26cb36382111
sayid <herq12315@gmail.com>
2020-01-16 18:16:42 +0800

    update htmlprofer url

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 jekyll build --config _config.yml
-htmlproofer ./_site --url-ignore www.youtube.com https://www.amazon.com
+htmlproofer ./_site --url-ignore www.youtube.com https://www.amazon.com/Permanent-Record-Edward-Snowden/dp/1250237238
