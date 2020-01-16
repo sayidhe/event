0a589a74925ac53429406c082181b68a326cbac5
sayid <herq12315@gmail.com>
2020-01-16 18:31:04 +0800

    add ssl_verifypeer: false

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 jekyll build --config _config.yml
-htmlproofer ./_site --url-ignore www.amazon.com/Permanent-Record-Edward-Snowden/dp/1250237238
+# htmlproofer ./_site --url-ignore www.amazon.com 
+HTML::Proofer.new("./_site", ssl_verifypeer: false).run
