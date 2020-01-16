f419ed5eb0d7b52ea66da7849c534561571c2507
sayid <herq12315@gmail.com>
2020-01-16 18:35:28 +0800

    test link htmlproofer url

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 jekyll build --config _config.yml
-# htmlproofer ./_site --url-ignore www.amazon.com 
-HTML::Proofer.new("./_site", ssl_verifypeer: false).run
+htmlproofer ./_site --url-ignore "https://www.amazon.com/Permanent-Record-Edward-Snowden/dp/1250237238"
